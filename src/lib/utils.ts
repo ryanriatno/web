60a41df9cb0b7016d6984aa9d894a90cfb32967c
Ryan Riatno <ryanriatno@gmail.com>
2025-04-22 11:39:14 +0700

    feat(utils): add tests and improve dateRange function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,18 +23,21 @@ export function readingTime(html: string) {
 export function dateRange(startDate: Date, endDate?: Date | string): string {
   const startMonth = startDate.toLocaleString("default", { month: "short" });
   const startYear = startDate.getFullYear().toString();
+  
+  if (!endDate) {
+    return `${startMonth} ${startYear} - Present`;
+  }
+
   let endMonth;
   let endYear;
 
-  if (endDate) {
-    if (typeof endDate === "string") {
-      endMonth = "";
-      endYear = endDate;
-    } else {
-      endMonth = endDate.toLocaleString("default", { month: "short" });
-      endYear = endDate.getFullYear().toString();
-    }
+  if (typeof endDate === "string") {
+    endMonth = "";
+    endYear = endDate;
+  } else {
+    endMonth = endDate.toLocaleString("default", { month: "short" });
+    endYear = endDate.getFullYear().toString();
   }
 
-  return `${startMonth}${startYear} - ${endMonth}${endYear}`;
+  return `${startMonth} ${startYear} - ${endMonth} ${endYear}`;
 }
